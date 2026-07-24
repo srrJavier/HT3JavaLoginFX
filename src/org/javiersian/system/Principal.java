9e4672eba8ed904bee204c42f9dee7d34bf6475e
Javier Sian <jsian-2026057@kinal.edu.gt>
2026-07-24 15:59:24 -0600

    000 se inicio el proyecto y se creo la clase Principal

---
+++ src/org/javiersian/system/Principal.java
@@ -0,0 +1,21 @@
+package org.javiersian.system;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Javier Sian
+ */
+
+public class Principal extends Application{
+    
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+        
+    }
+}
